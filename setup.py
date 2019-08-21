5ce328e2d2d6c3ebe366d767a5f2304d67f4bf89
Pedro Algarvio <pedro@algarvio.me>
2019-08-21 16:40:24 +0100

    Bump version for release

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ long_description = open("README.rst", "r", encoding="utf-8").read()
 
 setup(
     name="nox-py2",
-    version="2019.8.20",
+    version="2019.8.21",
     description="Flexible test automation.",
     long_description=long_description,
     url="https://nox.thea.codes",
